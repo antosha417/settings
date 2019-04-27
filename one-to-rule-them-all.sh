ac8b58b07532b4fbae02c2dc969b46d7cd4c7020
akovalkov <kovalkov.av@phystech.edu>
2019-04-27 10:08:20 +0300

    settings

--- one-to-rule-them-all.sh
+++ one-to-rule-them-all.sh
@@ -3,6 +3,7 @@ sudo python3 configure.py
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 chsh -s $(which zsh)
 
 firefox https://www.google.com/chrome/ https://www.jetbrains.com/idea/download
