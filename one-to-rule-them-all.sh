74dbaf73711b57cbb68e52e0221ba2c7f0afc0bd
akovalkov <kovalkov.av@phystech.edu>
2019-04-27 09:47:08 +0300

    settings

--- one-to-rule-them-all.sh
+++ one-to-rule-them-all.sh
@@ -1,6 +1,6 @@
 sudo python3 configure.py
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/ins    tall.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 chsh -s $(which zsh)
